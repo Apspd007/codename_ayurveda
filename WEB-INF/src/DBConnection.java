b9080075931aa88ac866ebdfe1a5d3ccaf8e9b43
iamthanendra <kashyapthanendra@gmail.com>
2023-04-15 11:02:55 -0700

    Added Medical-uses.html and Medical-place.html

--- WEB-INF/src/DBConnection.java
+++ WEB-INF/src/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection
         try{
             Class.forName("oracle.jdbc.driver.OracleDriver");
             //conn = DriverManager.getConnection("jdbc:oracle:thin:@Thanendra:1521:xe", "system", "system");
-            conn = DriverManager.getConnection("jdbc:oracle:thin:@DESKTOP-9QQ2DH6:1522:xe", "system", "user");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@DESKTOP-FCU04CN:1521:xe", "system", "user");
             return conn;
         }
         catch(Exception e)
